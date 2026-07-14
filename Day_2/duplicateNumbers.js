5fa1b983161886a3fd42ee98f7d658bdbfdb4ab9
Abdul-012 <abdulrahmankhan1757@gmail.com>
2026-07-14 19:23:36 +0530

    Revert "fix: improve duplicate number detection logic"
    This reverts commit 510056b72bbb3050f0b17c2a003574efd294230e.

--- Day_2/duplicateNumbers.js
+++ Day_2/duplicateNumbers.js
@@ -2,17 +2,13 @@
 function findDuplicates(arr) 
 {
     let seen = new Set();
-    let duplicates = new Set();
 
     for (let num of arr) 
     {
-        if (seen.has(num)) {
-            duplicates.add(num);
-        }
-        seen.add(num);
+            seen.add(num);
     }
-    return Array.from(duplicates);
-}
+    return seen;
+    }
 
 
 console.log(findDuplicates([1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9]));
