510056b72bbb3050f0b17c2a003574efd294230e
Abdul-012 <abdulrahmankhan1757@gmail.com>
2026-07-14 19:20:52 +0530

    fix: improve duplicate number detection logic

--- Day_2/duplicateNumbers.js
+++ Day_2/duplicateNumbers.js
@@ -2,13 +2,17 @@
 function findDuplicates(arr) 
 {
     let seen = new Set();
+    let duplicates = new Set();
 
     for (let num of arr) 
     {
-            seen.add(num);
+        if (seen.has(num)) {
+            duplicates.add(num);
+        }
+        seen.add(num);
     }
-    return seen;
-    }
+    return Array.from(duplicates);
+}
 
 
 console.log(findDuplicates([1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9]));
