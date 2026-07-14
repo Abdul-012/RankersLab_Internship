c5f6f64a30724dbfbfd31d1adde2dbccfca9a34b
Abdul-012 <abdulrahmankhan1757@gmail.com>
2026-07-14 19:23:36 +0530

    Revert "refactor: use Set for efficient vowel lookup"
    This reverts commit 2f793991b5ed830bc4d9cd75305ab55ed9348e32.

--- Day_1/countvowel.js
+++ Day_1/countvowel.js
@@ -1,11 +1,12 @@
 function countVowels(str) 
 {
-    const vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
     let count = 0;
 
     for (let char of str) 
-    {
-        if (vowels.has(char)) 
+        {
+        if( char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' ||
+            char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U'
+        ) 
         {
             count++;
         }
