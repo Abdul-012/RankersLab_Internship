c55663a42f9d154c5fec6f5b4c734e39bd0c7a99
Abdul-012 <abdulrahmankhan1757@gmail.com>
2026-07-14 19:24:07 +0530

    style: fix formatting and indentation in countVowels

--- Day_1/countvowel.js
+++ Day_1/countvowel.js
@@ -3,10 +3,9 @@ function countVowels(str)
     let count = 0;
 
     for (let char of str) 
-        {
-        if( char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' ||
-            char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U'
-        ) 
+    {
+        if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' ||
+            char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U') 
         {
             count++;
         }
